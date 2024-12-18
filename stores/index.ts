afe176f7a70386f9dcaff922c51f02830a0f9e77
Miguel Rodriguez <mrodrigueznav@gmail.com>
2024-12-18 06:59:36 -0600

    Pinia packages fix

--- stores/index.ts
+++ stores/index.ts
@@ -1,7 +1,8 @@
 import { createPinia } from 'pinia'
-import { createPersistedState } from 'pinia-plugin-persistedstate'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia()
-pinia.use(createPersistedState())
+pinia.use(piniaPluginPersistedstate)
+
 
 export default pinia
